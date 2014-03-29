f61552dae6d8861dd0c1e2d5bdc0ee4e88e333b0
PrashantLabh <prashantlabh604@gmail.com>
2014-03-30 00:21:52 +0530

    Adding Login page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,17 +4,34 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<div id="container">
+	<div class="signDiv">    
+		<form class="form-signin" id="loginFormInfo" method="post">    
+ 			<div class="frstPgLogo">   
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/yp.png" style="width:90px;height:60px;">  
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/nepal.gif" style="width:50px;height:60px;"> 
+			</div>    
+ 			<div class="dummyError alert alert-danger hideElement">
+       				Error messages shown here. This is hidden otherwise    
+ 			</div>    
+ 			<div class="input-group input-group-lg">     
+ 				<span class="input-group-addon">
+					<span class="glyphicon glyphicon-user"></span>
+				</span>     
+ 				<input type="text" class="form-control" name="username" id="idUser" placeholder="Username" autofocus="">     
+			</div>   
+ 			<div class="input-group input-group-lg">     
+ 				<span class="input-group-addon">
+					<span class="glyphicon glyphicon-log-out"></span>
+				</span>     
+ 				<input type="password" class="form-control" name="password" placeholder="Password">     
+			</div>     
+     
+			<input type="button" value="SIGN IN" id="signInBtn" class="btn btn-lg btn-option1">    
+ 			<div class="forgot" id="forgotPass">
+			<a href="#">I can't access my account, please help</a>
+			</div>   
+ 		</form> 
+	</div>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+	</div>
